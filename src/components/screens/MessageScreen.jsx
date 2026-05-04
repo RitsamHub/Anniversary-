3b94fa0d2144d834bf618fc98e6e639f95560351
RitsamHub <ritsam7890@gmail.com>
2026-05-04 23:30:45 +0530

    Update MessageScreen.jsx

--- src/components/screens/MessageScreen.jsx
+++ src/components/screens/MessageScreen.jsx
@@ -27,7 +27,12 @@ export default function MessageScreen() {
                 <div className={`cover ${opened ? "opacity-0" : "opacity-100"} pointer-events-none z-10 bg-[#ffedea]!`} />
 
                 <div className="relative px-6 h-56 overflow-y-auto text-foreground">
-                    Happy Birthday, Cutiepie! You deserve all the happiness, love, and smiles in the world today and always. You have this special way of making everything around you brighter, your smile, your kindness, and the way you make people feel truly cared for. I hope your day is filled with laughter, surprises, and moments that make your heart happy. You’re truly one of a kind, and I just want you to know how special you are. Keep being the amazing person you are, spreading joy wherever you go. Wishing you endless happiness, success, and all the sweet things life has to offer. 💗
+                    Happy First Anniversary, Cutiepie ❤️
+Aaj ka din hum dono ke liye bohot special hai. Shayad hum kabhi mile nahi, par jo connection aur pyaar hum share karte hain na, woh kisi distance se kam nahi hota. Tumhari baatein, tumhari care, aur tumhara hona hi meri life ko itna special bana deta hai.
+Long distance easy nahi hota, par tumhare saath sab kuch worth it lagta hai. Har call, har message, aur har chhoti si baat mere liye bohot important hai. Tum meri life ka woh part ho jo mujhe hamesha smile karne ki wajah deta hai.
+I wish ki jaldi woh din aaye jab hum finally mil sakein, bina screen ke, real mein… aur tab tak, main bas yahi chahta hoon ki hum aise hi ek dusre ke saath strong bane rahein.
+Thank you for loving me itna deeply, itna honestly. Tum sach mein one of a kind ho. ❤️
+Happy 1st Anniversary, jaan. Hamesha aise hi mere saath rehna. 💗 and last I love you meri pyaari si biwi 💞
                 </div>
             </div>
         </div>
